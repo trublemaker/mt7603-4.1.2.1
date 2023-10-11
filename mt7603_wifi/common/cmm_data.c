cdb8d463039639a8d5368115cfb09a92492e1565
mhl <mhl@sina.com>
2023-10-11 15:43:55 +0800

    Soft LED enable. Log info.

--- mt7603_wifi/common/cmm_data.c
+++ mt7603_wifi/common/cmm_data.c
@@ -26,6 +26,9 @@
 
 #include "rt_config.h"
 
+#ifdef MT76X3_AP_LED_SOFT
+#include <ralink/ralink_gpio.h>
+#endif
 
 UCHAR	SNAP_802_1H[] = {0xaa, 0xaa, 0x03, 0x00, 0x00, 0x00};
 UCHAR	SNAP_BRIDGE_TUNNEL[] = {0xaa, 0xaa, 0x03, 0x00, 0x00, 0xf8};
@@ -3797,7 +3800,10 @@ VOID RTMPDeQueuePacket(
 		releaseNetIf(&pAd->blockQueueTab[QueIdx]);
 	}
 #endif /* BLOCK_NET_IF */
-
+#if defined (MT76X3_AP_LED_SOFT)
+	if (Count > 0 && pAd->MacTab.Size > 0)
+		ralink_gpio_led_blink(LED_SOFT_BLINK_GPIO);
+#endif
 }
 
 
